14f39f92cf181ea61f23e81936634a6f6b7d10f8
earthhwachirawit@gmail.com <earthhwachirawit@gmail.com>
2025-04-03 08:36:50 +0700

    first_push

---
+++ src/lib/db.ts
@@ -0,0 +1,23 @@
+import sql from 'mssql';
+
+export const createConnection = async () => {
+  try {
+    const pool = await sql.connect({
+      user: 'sa',                    
+      password: 'B1mUmNU9',        
+      server: '192.168.120.9',    
+      database: 'DASHBOARD',        
+      options: {
+        encrypt: true,          
+        trustServerCertificate: true, 
+      },
+      connectionTimeout: 10000,     
+      requestTimeout: 15000,         
+    });
+
+    return pool;
+  } catch (err) {
+    console.error('Database connection error:', err);
+    throw err;
+  }
+};
