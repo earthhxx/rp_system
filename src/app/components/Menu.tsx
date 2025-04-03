ee8b900f8addebd90c5beba7db803031983bf652
earthhwachirawit@gmail.com <earthhwachirawit@gmail.com>
2025-04-03 19:43:57 +0700

    save

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const Menu = () => {
   return (
     <div className="relative justify-center items-center">
-      <div className="absolute top-100 left-0 h-50 w-15 rounded-2xl bg-black flex flex-col justify-center items-center">
-        <div className="flex flex-col text-center">
-          <span className="text-4xl font-extrabold"
-            style={{
-              writingMode: 'vertical-lr',
-              textOrientation: 'upright',
-            }}
-          >
-            MENU
-          </span>
+      <div className="absolute top-100 left-0 size-30 rounded-full bg-radial bg-black/10 backdrop-blur-3xl flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <div className="">
+          <Image
+              src="/images/LOGO3.png" 
+              alt="Menu Image" 
+              width={75} 
+              height={75} 
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen justify-center bg-amber-200">
-      <div className="flex w-1/2 text-3xl bg-black/50">
+    <div className="flex h-screen w-screen justify-center bg-white">
+      <div className="flex w-1/2 text-3xl ">
         <h1 className="text-center">
           TEST HOME PAGE
         </h1>
       </div>
-      <div className="flex w-1/2 text-3xl bg-black/50 ">
+      <div className="flex w-1/2 text-3xl  ">
 
       </div>
     </div>
