3062a2d7e82cfb5844bfa14c7d4f9ba026d5c211
earthhwachirawit@gmail.com <earthhwachirawit@gmail.com>
2025-04-04 09:21:50 +0700

    save

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -4,18 +4,28 @@ import Image from "next/image";
 const Menu = () => {
   return (
     <div className="relative justify-center items-center">
-      <div className="absolute top-100 left-0 size-30 rounded-full bg-radial bg-black/10 backdrop-blur-3xl flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
+      {/* Button im */}
+      <div className="absolute top-100 left-0 size-15 rounded-2xl bg-black/80 backdrop-blur-md shadow-md flex flex-col justify-center items-center drop-shadow-xl"> </div>
+      <div className="absolute top-100 left-0 size-15 rounded-2xl shadow-md flex flex-col justify-center items-center">
+        <div className="flex flex-col size-15 bg-white blur-[4] rounded-2xl justify-center items-center mr-[2px]">
           <div className="">
-          <Image
-              src="/images/LOGO3.png" 
-              alt="Menu Image" 
-              width={75} 
-              height={75} 
+            <Image
+              src="/images/LOGO3.png"
+              alt="Menu Image"
+              width={50}
+              height={50}
+              className=""
             />
           </div>
         </div>
       </div>
+
+      {/* MENU */}
+      <div className="absolute flex flex-col w-screen h-screen  justify-center items-center">
+        <div className=" flex size-150 rounded-2xl bg-black/50 backdrop-blur-md shadow-md justify-center items-center drop-shadow-xl"> </div>
+      </div>
+
+
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
+import Image from "next/image";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen justify-center bg-white">
-      <div className="flex w-1/2 text-3xl ">
-        <h1 className="text-center">
-          TEST HOME PAGE
-        </h1>
-      </div>
-      <div className="flex w-1/2 text-3xl  ">
-
-      </div>
+    <div className="flex h-screen w-screen justify-center items-center bg-blue-100">
+        <Image
+                      src="/images/bg.jpg"
+                      alt="Menu Image"
+                      width={50}
+                      height={50}
+                      className="w-screen h-screen"
+                    />
     </div>
   );
 }
