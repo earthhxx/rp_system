1be4181243818979bfdcebabcc4e0deb80f7c7ca
earthhxx <earthhwachirawit@gmail.com>
2025-06-19 12:17:25 +0700

    save

--- src/lib/connection.ts
+++ src/lib/connection.ts
@@ -1,16 +1,19 @@
 import sql from 'mssql';
+import dotenv from 'dotenv';
+
+dotenv.config(); // โหลด .env
 
 let pool_NewFCXT: sql.ConnectionPool | null = null;
 let pool_DASHBOARD: sql.ConnectionPool | null = null;
 
 export const getNewFCXTConnection = async () => {
   if (pool_NewFCXT) return pool_NewFCXT;
-  
+
   pool_NewFCXT = await new sql.ConnectionPool({
-    user: 'sa',
-    password: 'B1mUmNU9',
-    server: '192.168.120.2',
-    database: 'NewFCXT(IM Thailand)',
+    user: process.env.DB1_USER!,
+    password: process.env.DB1_PASSWORD!,
+    server: process.env.DB1_SERVER!,
+    database: process.env.DB1_NAME!,
     options: {
       encrypt: false,
       trustServerCertificate: true,
@@ -25,10 +28,10 @@ export const getDashboardConnection = async () => {
   if (pool_DASHBOARD) return pool_DASHBOARD;
 
   pool_DASHBOARD = await new sql.ConnectionPool({
-    user: 'sa',
-    password: 'B1mUmNU9',
-    server: '192.168.120.9',
-    database: 'DASHBOARD',
+    user: process.env.DB2_USER!,
+    password: process.env.DB2_PASSWORD!,
+    server: process.env.DB2_SERVER!,
+    database: process.env.DB2_NAME!,
     options: {
       encrypt: false,
       trustServerCertificate: true,
