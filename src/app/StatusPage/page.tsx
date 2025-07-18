5488f9c4f76314c4a602e231fedd468c7c776c05
earthhxx <earthhwachirawit@gmail.com>
2025-07-18 07:56:45 +0700

    save

--- src/app/StatusPage/page.tsx
+++ src/app/StatusPage/page.tsx
@@ -1467,7 +1467,7 @@ const checkreflowpage = () => {
                   type="password"
                   autoComplete="off"
                   value={EmployeeNo}
-                  onChange={(e) => { handleChangeInputID }}
+                  onChange={ handleChangeInputID }
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg m-4 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="รหัสพนักงาน"
                 />
@@ -1553,7 +1553,7 @@ const checkreflowpage = () => {
                   ref={inputRef}
                   type="text"
                   autoComplete="off"
-                  onChange={(e) => { handleChangeInputID }}
+                  onChange={ handleChangeInputID }
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg m-4 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="รหัสพนักงาน"
                 />
