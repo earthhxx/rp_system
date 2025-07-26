48b5308aee5e15756db9ec58683e329e06a4ef87
earthhxx <earthhwachirawit@gmail.com>
2025-07-26 14:36:01 +0700

    save

--- src/app/StatusPage/page.tsx
+++ src/app/StatusPage/page.tsx
@@ -815,7 +815,6 @@ const PageStatus = () => {
                                 ref={inputRef}
                                 type="password"
                                 autoComplete="off"
-                                value={EmployeeNo}
                                 onChange={handleChangeInputID}
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg m-4 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="รหัสพนักงาน"
